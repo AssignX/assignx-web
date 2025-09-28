a2db199cc43753aac5aa2b77ad9acfbed8c32e3a
gonu <90233723+makisepel@users.noreply.github.com>
2025-09-28 15:50:45 +0900

    chore: Edit line height values for each p tag

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,18 +1,20 @@
 import PropTypes from 'prop-types';
 import SelectionForm from './SelectionForm';
+// import Button from './Button';
 function PageHeader({
   title,
   helperText,
   hasConfirmSelection = false,
   selected,
   setSelected,
+  //   buttonsData = [],
 }) {
   return (
     <div className='border-border-sectionbottom flex items-center border-b pb-[10px]'>
       <div className='flex w-full flex-col gap-[5px]'>
         <div className='flex flex-row gap-[10px]'>
           <p
-            className='text-text-main font-bold'
+            className='text-text-main leading-[23px] font-bold'
             style={{ fontSize: '19.5px' }}
           >
             {title}
@@ -27,12 +29,15 @@ function PageHeader({
           )}
         </div>
         {helperText && (
-          <p className='text-blue' style={{ fontSize: '13px' }}>
+          <p
+            className='text-blue leading-[16px] font-bold'
+            style={{ fontSize: '13px' }}
+          >
             {helperText}
           </p>
         )}
       </div>
-      {/* Buttons */}
+      <div className='gap-[8px]'>{/* Buttons */}</div>
     </div>
   );
 }
