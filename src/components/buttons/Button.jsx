33e6eb4f1e6d03e0536b74fad00226dbb5c6acd8
jeonghoon <advancedtrio@naver.com>
2025-09-29 00:19:53 +0900

    fix: Modify Button, ButtonGroup, ButtonTestPage problems comfirmed by prettier, ESLint

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * Button 컴포넌트
@@ -10,58 +10,53 @@ import PropTypes from "prop-types";
  * @param {function} onClick - 클릭 이벤트 핸들러
  */
 
-
 export default function Button({
-    text = "button",
-    color = "gray", // red | 
-    textSize = "text-sm",
-    Icon,
-    onClick,
-}){
-    // 색상 매핑
-    const colors = {
-        red: {
-            bg: "bg-[var(--color-red)]",
-            text: "text-[var(--color-white)]",
-            hover: "hover:opacity-90",
-        },
-        lightgray: {
-            bg: "bg-[var(--color-light-gray)]",
-            text: "text-black",
-            hover: "hover:opacity-90",
-        },
-        gold: {
-            bg: "bg-[var(--color-gold)]",
-            text: "text-[var(--color-white)]",
-            hover: "hover:opacity-90",
-        }
-    };
+  text = 'button',
+  color = 'gray', // red |
+  textSize = 'text-sm',
+  Icon,
+  onClick,
+}) {
+  // 색상 매핑
+  const colors = {
+    red: {
+      bg: 'bg-[var(--color-red)]',
+      text: 'text-[var(--color-white)]',
+      hover: 'hover:opacity-90',
+    },
+    lightgray: {
+      bg: 'bg-[var(--color-light-gray)]',
+      text: 'text-black',
+      hover: 'hover:opacity-90',
+    },
+    gold: {
+      bg: 'bg-[var(--color-gold)]',
+      text: 'text-[var(--color-white)]',
+      hover: 'hover:opacity-90',
+    },
+  };
 
-    const selected = colors[color] || colors.red;
+  const selected = colors[color] || colors.red;
 
-    return(
-        <button
-            type="button"
-            onClick={onClick}
-            style={{ cursor: 'pointer' }}
-            className={`
-                flex items-center justify-center gap-1
-                ${selected.bg} ${selected.text} ${selected.hover}
-                w-auto h-[30px] ${textSize}
-                ${Icon ? "px-[10px]" : "px-4"}
-                focus:ring-0 focus:ouline-none font-normal rounded-none
-                transition duration-200`}
-        >
-            {Icon && <Icon className="w-4 h-4 relative top-[1px]" aria-hidden="true" />}
-            {text}
-        </button>
-    );
+  return (
+    <button
+      type='button'
+      onClick={onClick}
+      style={{ cursor: 'pointer' }}
+      className={`flex items-center justify-center gap-1 ${selected.bg} ${selected.text} ${selected.hover}w-auto h-[30px] ${textSize} ${Icon ? 'px-[10px]' : 'px-4'} ouline-none rounded-none font-normal transition duration-200 focus:ring-0`}
+    >
+      {Icon && (
+        <Icon className='relative top-[1px]' w-4 h-4 aria-hidden='true' />
+      )}
+      {text}
+    </button>
+  );
 }
 
-Button.PropTypes = {
-    text: PropTypes.string.isRequired, // 반드시 문자열
-    color: PropTypes.oneOf(["red", "lightgray", "gold"]), // 3개 중 하나
-    textSize: PropTypes.string, // Tailwind 사이즈 클래스
-    Icon: PropTypes.elementType, // react component
-    onClick: PropTypes.func, // 함수 -  클릭 이벤트 핸들러
+Button.propTypes = {
+  text: PropTypes.string.isRequired, // 반드시 문자열
+  color: PropTypes.oneOf(['red', 'lightgray', 'gold']), // 3개 중 하나
+  textSize: PropTypes.string, // Tailwind 사이즈 클래스
+  Icon: PropTypes.elementType, // react component
+  onClick: PropTypes.func, // 함수 -  클릭 이벤트 핸들러
 };
--- src/components/buttons/ButtonGroup.jsx
+++ src/components/buttons/ButtonGroup.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Button from "./Button";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
 
 /**
  * ButtonGroup 컴포넌트
@@ -10,35 +10,36 @@ import Button from "./Button";
  */
 
 export default function ButtonGroup({
-    buttons=[],
-    direction = "row", // "row" || "col"
-    gap = "gap-[5px]",
+  buttons = [],
+  direction = 'row', // "row" || "col"
+  gap = 'gap-[5px]',
 }) {
-    return (
-        <div className={`flex ${direction === "col" ?  "flex-col" : "flex-row"} ${gap}`}>
-            {buttons.map((btn, index) => (
-                <Button
-                    key={index}
-                    text={btn.text}
-                    color={btn.color}
-                    Icon={btn.Icon}
-                    onClick={btn.onClick}
-                    />
-            ))}
-        </div>
-    );
+  return (
+    <div
+      className={`flex ${direction === 'col' ? 'flex-col' : 'flex-row'}${gap}`}
+    >
+      {buttons.map((btn, index) => (
+        <Button
+          key={index}
+          text={btn.text}
+          color={btn.color}
+          Icon={btn.Icon}
+          onClick={btn.onClick}
+        />
+      ))}
+    </div>
+  );
 }
-
 
 ButtonGroup.propTypes = {
   buttons: PropTypes.arrayOf(
     PropTypes.shape({
       text: PropTypes.string.isRequired,
-      color: PropTypes.oneOf(["red", "lightgray", "gold"]),
+      color: PropTypes.oneOf(['red', 'lightgray', 'gold']),
       Icon: PropTypes.elementType,
       onClick: PropTypes.func,
     })
   ), // 버튼 리스트
-  direction: PropTypes.oneOf(["row", "col"]), // row 또는 col
+  direction: PropTypes.oneOf(['row', 'col']), // row 또는 col
   gap: PropTypes.string, // tailwind gap 클래스
 };
