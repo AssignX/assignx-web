7d005394c9acaf829477b58b980acedc09382ab9
gonu <90233723+makisepel@users.noreply.github.com>
2025-09-28 15:36:21 +0900

    feat: Add ConfirmSelection in PageHeader

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,20 +1,38 @@
 import PropTypes from 'prop-types';
-
-function PageHeader({ title, helperText }) {
+import SelectionForm from './SelectionForm';
+function PageHeader({
+  title,
+  helperText,
+  hasConfirmSelection = false,
+  selected,
+  setSelected,
+}) {
   return (
     <div className='border-border-sectionbottom flex items-center border-b pb-[10px]'>
-      <div className='flex flex-col gap-[5px]'>
-        <div className='flex flex-row gap-[5px]'>
-          <h1
+      <div className='flex w-full flex-col gap-[5px]'>
+        <div className='flex flex-row gap-[10px]'>
+          <p
             className='text-text-main font-bold'
             style={{ fontSize: '19.5px' }}
           >
             {title}
-          </h1>
-          {/* ConfirmSelection */}
+          </p>
+          {hasConfirmSelection && (
+            <SelectionForm
+              leftText='확정'
+              rightText='미확정'
+              selected={selected}
+              setSelected={setSelected}
+            />
+          )}
         </div>
-        {helperText && <p className='text-blue'>{helperText}</p>}
+        {helperText && (
+          <p className='text-blue' style={{ fontSize: '13px' }}>
+            {helperText}
+          </p>
+        )}
       </div>
+      {/* Buttons */}
     </div>
   );
 }
@@ -22,8 +40,9 @@ function PageHeader({ title, helperText }) {
 PageHeader.propTypes = {
   title: PropTypes.string.isRequired,
   helperText: PropTypes.string,
-  //   hasConfirmSelection: PropTypes.bool,
-  //   onConfirmSelection: PropTypes.func,
+  hasConfirmSelection: PropTypes.bool,
+  selected: PropTypes.bool,
+  setSelected: PropTypes.func,
   //   buttonsData: PropTypes.arrayOf(PropTypes.node),
 };
 
