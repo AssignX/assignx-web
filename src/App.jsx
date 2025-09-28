7e56b8786d91a1c9f2ca63066e867d2e05e7a0e4
jeonghoon <advancedtrio@naver.com>
2025-09-28 17:06:50 +0900

    feat: Add button component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './styles/font.css';
 import SyPage from './pages/sy/Sy.jsx';
 import AdminHomePage from './pages/admin/Home.jsx';
 import { SearchIcon, SaveIcon, PlusIcon } from '@/assets/icons';
+import Button from '@/assets/buttons/button.jsx';
 
 export default function App() {
   return (
@@ -19,6 +20,32 @@ export default function App() {
             <SaveIcon width={40} height={40} />
             <PlusIcon width={40} height={40} />
           </div>
+        }
+      />
+      <Route
+        path='buttontest'
+        element={
+          <div>
+            <h1>button</h1>
+            <div className="flex gap-1">
+              <Button text="신청" color="red" />
+              <Button text="수정" color="red" />
+              <Button text="삭제" color="red" />
+              </div>
+
+            <div className="flex gap-1">
+              <Button text="조회" color="gold" />
+              <Button text="추가" color="gold" />
+              <Button text="버튼" color="gold" />
+              <Button text="삭제" color="gold" />
+            </div>
+
+            <div className="flex gap-1">
+              <Button text="저장" Icon={SaveIcon} color="lightgray" />
+              <Button text="조회" Icon={SearchIcon} color="lightgray" />
+            </div>
+          </div>
+
         }
       />
     </Routes>
---
+++ src/assets/buttons/button.jsx
@@ -0,0 +1,48 @@
+import React from "react"
+
+
+export default function Button({
+    text = "button",
+    color = "gray", // red | 
+    textSize = "text-sm",
+    Icon,
+    onClick,
+}){
+    // 색상 매핑
+    const colors = {
+        red: {
+            bg: "bg-[var(--color-red)]",
+            text: "text-[var(--color-white)]",
+            hover: "hover:opacity-90",
+        },
+        lightgray: {
+            bg: "bg-[var(--color-light-gray)]",
+            text: "text-black",
+            hover: "hover:opacity-90",
+        },
+        gold: {
+            bg: "bg-[var(--color-gold)]",
+            text: "text-[var(--color-white)]",
+            hover: "hover:opacity-90",
+        }
+    };
+
+    const selected = colors[color] || colors.red;
+
+    return(
+        <button
+            type="button"
+            onClick={onClick}
+            className={`
+                flex items-center justify-center gap-1
+                ${selected.bg} ${selected.text} ${selected.hover}
+                w-auto h-[30px] ${textSize}
+                ${Icon ? "px-[10px]" : "px-4"}
+                focus:ring-0 focus:ouline-none font-normal rounded-none
+                transition duration-200`}
+        >
+            {Icon && <Icon className="w-4 h-4 relative top-[1px]" aria-hidden="true" />}
+            {text}
+        </button>
+    );
+}
