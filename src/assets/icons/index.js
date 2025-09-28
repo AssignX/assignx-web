f14406faf10bf8156e0da69f5b8d3a4a20c186db
gonu <90233723+makisepel@users.noreply.github.com>
2025-09-28 22:23:55 +0900

    feat: Add house icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -5,3 +5,4 @@ export { default as PlusIcon } from './plus.svg?react';
 export { default as CheckIcon } from './check.svg?react';
 export { default as ChevronLeftIcon } from './chevron-left.svg?react';
 export { default as ChevronRightIcon } from './chevron-right.svg?react';
+export { default as HouseIcon } from './house.svg?react';
