54d13ece94191273f76b5f5b41e99fed9d569f76
gonu <90233723+makisepel@users.noreply.github.com>
2025-09-28 16:41:45 +0900

    feat: Add calculating functions in WeekPicker

--- src/components/WeekPicker.jsx
+++ src/components/WeekPicker.jsx
@@ -1,19 +1,38 @@
 import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
+import isoWeek from 'dayjs/plugin/isoWeek';
 import { ChevronLeftIcon, ChevronRightIcon } from '@/assets/icons';
 
-function WeekPicker() {
+dayjs.extend(isoWeek);
+
+// without onOpenPicker prop
+function WeekPicker({ date, setDate }) {
+  const goPrev = () => setDate(date.add(-7, 'day'));
+  const goNext = () => setDate(date.add(7, 'day'));
+
+  const getWeekLabel = (d) => {
+    const year = d.year();
+    const month = d.month() + 1;
+
+    const firstDayOfMonth = dayjs(`${year}-${month}-01`);
+    const weekOfMonth = d.isoWeek() - firstDayOfMonth.isoWeek() + 1;
+
+    return `${year}년 ${month}월 ${weekOfMonth}주차`;
+  };
+
+  const label = getWeekLabel(date);
   return (
     <div className='text-text-main flex h-[20px] items-center gap-[10px]'>
-      <button style={{ cursor: 'pointer' }} onClick={() => {}}>
+      <button style={{ cursor: 'pointer' }} onClick={goPrev}>
         <ChevronLeftIcon />
       </button>
       <span
         className='text-[14px] leading-[17px] font-medium'
         style={{ cursor: 'pointer' }}
       >
-        2025년 9월 2주차
+        {label}
       </span>
-      <button style={{ cursor: 'pointer' }} onClick={() => {}}>
+      <button style={{ cursor: 'pointer' }} onClick={goNext}>
         <ChevronRightIcon />
       </button>
     </div>
@@ -21,8 +40,9 @@ function WeekPicker() {
 }
 
 WeekPicker.propTypes = {
-  selectedWeek: PropTypes.string.isRequired,
-  setSelectedWeek: PropTypes.func.isRequired,
+  date: PropTypes.object.isRequired,
+  setDate: PropTypes.func.isRequired,
+  //   onOpenPicker: PropTypes.func,
 };
 
 export default WeekPicker;
