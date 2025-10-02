7c2bb44f537473109ae841691325cc00a72ed61e
EunjeongKim <kimeunjeong0821@gmail.com>
2025-10-02 20:53:01 +0900

    test: Add 1st Exam Table

---
+++ src/components/table/Prof_1stExamTable.jsx
@@ -0,0 +1,52 @@
+import HorizontalTable from './HorizontalTable';
+
+export default function Prof_1stExamTable() {
+  const allItemsInOneRow = [
+    {
+      id: 'year',
+      label: '개설년도',
+      labelWidth: '120px',
+      contentWidth: '180px',
+      required: true,
+      content: '2025',
+    },
+    {
+      id: 'semester',
+      label: '개설학기',
+      labelWidth: '120px',
+      contentWidth: '180px',
+      required: true,
+      content: '2학기',
+    },
+    {
+      id: 'department',
+      label: '소속학과',
+      labelWidth: '120px',
+      contentWidth: '300px',
+      content: 'IT대학 컴퓨터학부',
+    },
+    {
+      id: 'studentId',
+      label: '학번',
+      labelWidth: '120px',
+      contentWidth: '180px',
+      required: true,
+      content: '95-120392',
+    },
+    {
+      id: 'name',
+      label: '이름',
+      labelWidth: '120px',
+      contentWidth: '180px',
+      required: true,
+      content: '교수1',
+    },
+  ];
+
+  return (
+    <div>
+      <h1 className='mb-4 text-xl font-bold'>1차 시험 정보</h1>
+      <HorizontalTable items={allItemsInOneRow} />
+    </div>
+  );
+}
