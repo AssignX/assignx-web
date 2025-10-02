740e8aa9762f12a145fdc923ce248781aea941fb
EunjeongKim <kimeunjeong0821@gmail.com>
2025-10-02 20:49:29 +0900

    chore: Modify from Internal State Management to Control Components

--- src/components/table/cells/InputCell.jsx
+++ src/components/table/cells/InputCell.jsx
@@ -1,52 +1,39 @@
-import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 export function InputCell({
-  initialValue,
-  rowId,
-  columnKey,
-  updateData,
+  value,
+  onChange,
+  onKeyDown,
+  onBlur,
   disabled,
+  className,
 }) {
-  const [value, setValue] = useState(initialValue);
-
-  useEffect(() => {
-    setValue(initialValue);
-  }, [initialValue]);
-
-  const onChange = (e) => {
-    setValue(e.target.value);
-  };
-
-  const onBlur = () => {
-    // Only update if the value has changed
-    if (initialValue !== value) {
-      updateData(rowId, columnKey, value);
-    }
-  };
-
-  const inputClassName = `h-8 w-full border border-table-border px-2.5 py-2.5 text-sm ${
+  const defaultClassName = `h-8 w-full border border-table-border p-2.5 text-sm ${
     disabled ? 'bg-inputCell-disabled' : ''
   }`;
+
+  const finalClassName = `${defaultClassName} ${className || ''}`.trim();
 
   return (
     <input
       type='text'
       value={value}
       onChange={onChange}
+      onKeyDown={onKeyDown}
       onBlur={onBlur}
       disabled={disabled}
-      className={inputClassName.trim()}
+      className={finalClassName}
     />
   );
 }
 
 InputCell.propTypes = {
-  initialValue: PropTypes.string.isRequired,
-  rowId: PropTypes.string.isRequired,
-  columnKey: PropTypes.string.isRequired,
-  updateData: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onKeyDown: PropTypes.func,
+  onBlur: PropTypes.func,
   disabled: PropTypes.bool,
+  className: PropTypes.string,
 };
 
 InputCell.defaultProps = { disabled: false };
