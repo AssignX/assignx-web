e521c52c70bed5c063a982483c96a872fcaa0e70
EunjeongKim <kimeunjeong0821@gmail.com>
2025-10-09 04:28:56 +0900

    Feat: Get height as props

--- src/components/table/cells/InputCell.jsx
+++ src/components/table/cells/InputCell.jsx
@@ -1,14 +1,26 @@
 import PropTypes from 'prop-types';
 
-export function InputCell({
+/**
+ * 테이블 셀 내에서 사용되는 기본 텍스트 입력(input) 컴포넌트
+ * @param {string} value - 입력 필드의 현재 값 (필수)
+ * @param {function} onChange - 입력 값이 변경될 때 호출되는 콜백 함수 (필수)
+ * @param {function} onKeyDown - 키보드 키가 눌렸을 때 호출되는 콜백 함수 (선택)
+ * @param {function} onBlur - 입력 필드가 포커스를 잃었을 때 호출되는 콜백 함수 (선택)
+ * @param {boolean} disabled - 입력 필드를 비활성화할지 여부 (기본값: false)
+ * @param {string} className - 추가적으로 적용할 Tailwind CSS 클래스 문자열 (선택)
+ * @param {number} height - 입력 필드의 높이(px). (기본값: 26px(row=35px일 때), sy페이지만 32px로 설정(row=41px일 때))
+ */
+
+export default function InputCell({
   value,
   onChange,
   onKeyDown,
   onBlur,
   disabled,
   className,
+  height = 26,
 }) {
-  const defaultClassName = `h-8 w-full border border-table-border p-2.5 text-[13px] ${
+  const defaultClassName = `w-full border border-table-border p-2.5 text-[13px] ${
     disabled ? 'bg-inputCell-disabled' : ''
   }`;
 
@@ -23,6 +35,7 @@ export function InputCell({
       onBlur={onBlur}
       disabled={disabled}
       className={finalClassName}
+      style={{ height: `${height}px` }}
     />
   );
 }
@@ -34,6 +47,7 @@ InputCell.propTypes = {
   onBlur: PropTypes.func,
   disabled: PropTypes.bool,
   className: PropTypes.string,
+  height: PropTypes.number,
 };
 
 InputCell.defaultProps = { disabled: false };
--- src/components/table/cells/SearchCell.jsx
+++ src/components/table/cells/SearchCell.jsx
@@ -1,9 +1,16 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { SearchIcon } from '@/assets/icons';
-import { InputCell } from './InputCell';
+import InputCell from './InputCell';
 
-export function SearchCell({ initialValue, onSearch }) {
+/**
+ * 검색 아이콘 버튼이 결합된 텍스트 입력(input) 컴포넌트
+ * @param {string} initialValue - 입력 필드의 초기 값 (기본값: '')
+ * @param {function} onSearch - 검색 버튼을 클릭하거나 Enter 키를 눌렀을 때 호출되는 콜백 함수. 현재 입력 값을 인자로 받음 (필수)
+ * @param {number} height - 입력 필드와 검색 버튼의 높이(px) (기본값: 26px(row=35px일 때), sy페이지만 32px로 설정(row=41px일 때))
+ */
+
+export default function SearchCell({ initialValue, onSearch, height = 26 }) {
   const [value, setValue] = useState(initialValue);
 
   useEffect(() => {
@@ -32,11 +39,13 @@ export function SearchCell({ initialValue, onSearch }) {
         value={value}
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
+        height={height}
       />
       <button
         type='button'
         onClick={handleSearchClick}
-        className='border-table-border flex h-8 w-8 flex-shrink-0 items-center justify-center border-y border-r p-1'
+        className='border-table-border flex flex-shrink-0 items-center justify-center border-y border-r p-1'
+        style={{ height: `${height}px`, width: `${height}px` }}
         aria-label='Search'
       >
         <SearchIcon />
@@ -48,6 +57,7 @@ export function SearchCell({ initialValue, onSearch }) {
 SearchCell.propTypes = {
   initialValue: PropTypes.string,
   onSearch: PropTypes.func.isRequired,
+  height: PropTypes.number,
 };
 
 SearchCell.defaultProps = { initialValue: '' };
