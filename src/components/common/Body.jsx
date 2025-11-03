1b9f76a95558bcf33635fb4e39873eff713bcedf
gonu <90233723+makisepel@users.noreply.github.com>
2025-11-03 19:37:41 +0900

    chore: Change body arrangement

--- src/components/common/Body.jsx
+++ src/components/common/Body.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export default function Body({ children }) {
   return (
-    <div className='flex h-full w-full flex-row items-center justify-start gap-[20px] bg-[#f5f6f8] p-[20px]'>
+    <div className='flex h-full w-full flex-row items-start justify-center gap-[20px] bg-[#f5f6f8] p-[20px]'>
       {children}
     </div>
   );
