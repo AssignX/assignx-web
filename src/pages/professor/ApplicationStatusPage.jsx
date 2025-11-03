e441fa48a5c97b92a421df3c246cd7d995c6bb2b
gonu <90233723+makisepel@users.noreply.github.com>
2025-11-03 19:46:52 +0900

    feat: Add headers in StatusPage

--- src/pages/professor/ApplicationStatusPage.jsx
+++ src/pages/professor/ApplicationStatusPage.jsx
@@ -1,7 +1,21 @@
 import Section from '@/components/common/Section';
+import PageHeader from '@/components/headers/PageHeader';
+import SectionHeader from '@/components/headers/SectionHeader';
 
 function ApplicationStatusPage() {
-  return <Section>sect</Section>;
+  const subTitle = 'N건';
+  return (
+    <Section>
+      <PageHeader title='시간표 조회' />
+      {/* 가로 폼 */}
+
+      <SectionHeader title='과목 조회 목록' subtitle={subTitle} />
+      {/* 테이블 */}
+
+      <SectionHeader title='강의 시간표' />
+      {/* 타임테이블 */}
+    </Section>
+  );
 }
 
 export default ApplicationStatusPage;
