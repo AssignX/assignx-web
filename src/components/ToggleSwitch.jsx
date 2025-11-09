f1314efed9cb03d8892cbedb251f0010f3af7f92
EunjeongKim <kimeunjeong0821@gmail.com>
2025-11-10 03:37:36 +0900

    feat: add ToggleSwitch Component

---
+++ src/components/ToggleSwitch.jsx
@@ -0,0 +1,22 @@
+// src/components/common/ToggleSwitch.jsx
+import PropTypes from 'prop-types';
+
+export default function ToggleSwitch({ checked, onChange }) {
+  return (
+    <label className='flex cursor-pointer items-center select-none'>
+      <div
+        className={`border-table-border flex h-5 w-10 items-center rounded-full border p-1 transition-colors duration-300 ${checked ? 'bg-red' : 'bg-white'}`}
+        onClick={() => onChange(!checked)}
+      >
+        <div
+          className={`h-4 w-4 transform rounded-full bg-white shadow-md transition-transform duration-300 ${checked ? 'translate-x-5' : 'translate-x-0'}`}
+        />
+      </div>
+    </label>
+  );
+}
+
+ToggleSwitch.propTypes = {
+  checked: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
