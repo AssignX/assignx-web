585b7b571c91b80b040101e53a9862a05c691886
gonu <90233723+makisepel@users.noreply.github.com>
2025-11-17 14:11:37 +0900

    feat: Add Sidebar's isSelected logic in Professor

--- src/pages/professor/Professor.jsx
+++ src/pages/professor/Professor.jsx
@@ -1,24 +1,33 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Sidebar from '@/components/Sidebar';
 import NavBar from '@/components/NavBar';
 import Body from '@/components/common/Body';
 import PageWrapper from '@/components/common/PageWrapper';
 
 function Professor() {
-  const professorMenus = [
+  const location = useLocation();
+  const baseMenus = [
     {
       title: '강의 조회',
-      subItems: [{ label: '시간표 조회', path: 'subject', isSelected: true }],
+      subItems: [{ label: '시간표 조회', path: 'subject' }],
     },
     {
       title: '시험 신청',
       subItems: [
-        { label: '1차 시험 신청', path: 'first', isSelected: false },
-        { label: '2차 시험 신청', path: 'second', isSelected: false },
-        { label: '신청 현황 조회', path: 'status', isSelected: false },
+        { label: '1차 시험 신청', path: 'first' },
+        { label: '2차 시험 신청', path: 'second' },
+        { label: '신청 현황 조회', path: 'status' },
       ],
     },
   ];
+
+  const professorMenus = baseMenus.map((menu) => ({
+    ...menu,
+    subItems: menu.subItems.map((sub) => ({
+      ...sub,
+      isSelected: location.pathname.startsWith(`/professor/${sub.path}`),
+    })),
+  }));
 
   return (
     <PageWrapper>
