39aa592bd1ff824bcf63b2bc634344d22e25cc49
gonu <90233723+makisepel@users.noreply.github.com>
2025-11-22 13:02:34 +0900

    chore: Add border in Section component

--- src/components/layout/Section.jsx
+++ src/components/layout/Section.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export default function Section({ children }) {
   return (
-    <div className='flex h-full w-full flex-col items-stretch justify-start gap-[20px] px-[20px] py-[40px]'>
+    <div className='border-border-contents flex h-full w-full flex-col items-stretch justify-start gap-[20px] border px-[20px] py-[40px]'>
       {children}
     </div>
   );
